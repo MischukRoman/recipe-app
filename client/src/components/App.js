8ea0696142e43bf056e76b788fdebf0c4f2be433
Mischuk Roman <mischuk.romann@gmail.com>
2020-10-13 22:09:45 +0300

    Added animation to RecipeList and RecipeItem

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import posed from 'react-pose';
 import './App.css';
 
 import {Query} from 'react-apollo'
 import {GET_ALL_RECIPES} from '../queries'
 import RecipeItem from "./Recipe/RecipeItem";
 
-const App = () => (
-    <div className="App">
-        <h1 className="main-title">
-            Find Recipes You <strong>Love</strong>
-        </h1>
-        <Query query={GET_ALL_RECIPES}>
-            {({data, loading, error}) => {
-                if (loading) return <div>Loading...</div>;
-                if (error) return <div>Error</div>;
-                return (
-                    <ul className="cards">
-                        {data.getAllRecipes.map((recipe) => (
-                            <RecipeItem key={recipe._id} {...recipe}/>
-                        ))}
-                    </ul>
-                )
-            }}
-        </Query>
-    </div>
-);
+const RecipeList = posed.ul({
+    shown: {
+        x: '0%',
+        staggerChildren: 100
+    },
+    hidden: {
+        x: '-100%'
+    }
+});
+
+const App = () => {
+    const [state, setState] = useState({
+        on: false
+    });
+
+    useEffect(() => {
+        setTimeout(slideIn, 200)
+    }, []);
+
+    const slideIn = () => {
+        setState(prevData => ({...prevData, on: !prevData.on}))
+    };
+
+    return (
+        <div className="App">
+            <h1 className="main-title">
+                Find Recipes You <strong>Love</strong>
+            </h1>
+            <Query query={GET_ALL_RECIPES}>
+                {({data, loading, error}) => {
+                    if (loading) return <div>Loading...</div>;
+                    if (error) return <div>Error</div>;
+
+                    const {on} = state;
+                    return (
+                        <RecipeList
+                            pose={on ? 'shown' : 'hidden'}
+                            className="cards"
+                        >
+                            {data.getAllRecipes.map((recipe) => (
+                                <RecipeItem key={recipe._id} {...recipe}/>
+                            ))}
+                        </RecipeList>
+                    )
+                }}
+            </Query>
+        </div>
+    );
+};
 
 export default App;
