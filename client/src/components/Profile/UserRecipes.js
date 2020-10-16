1ab580dccb7b41d77c5fd0f931c54858764df2d2
Mischuk Roman <mischuk.romann@gmail.com>
2020-10-17 01:03:44 +0300

    added edit recipe button and modal

--- client/src/components/Profile/UserRecipes.js
+++ client/src/components/Profile/UserRecipes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import {Query, Mutation} from "react-apollo";
+
 import {DELETE_USER_RECIPE, GET_ALL_RECIPES, GET_CURRENT_USER, GET_USER_RECIPES} from "../../queries";
 import Spinner from "../Spinner";
 
@@ -22,6 +23,7 @@ const UserRecipes = ({username}) => {
                 if (error) return <div>Error</div>;
                 return (
                     <ul>
+                        <EditRecipeModal/>
                         <h3>Your Recipes</h3>
                         {!data.getUserRecipes.length && <p><strong>You have not added any recipes yet</strong></p>}
                         {
@@ -55,10 +57,13 @@ const UserRecipes = ({username}) => {
 
                                                   }}>
                                             {(deleteUserRecipe, attrs = {}) => (
-                                                <p onClick={() => handleDelete(deleteUserRecipe)}
-                                                   className="delete-button">
-                                                    {attrs.loading ? 'loading...' : 'X'}
-                                                </p>
+                                                <div>
+                                                    <button className={"button-primary"}>Update</button>
+                                                    <p onClick={() => handleDelete(deleteUserRecipe)}
+                                                       className={"delete-button"}>
+                                                        {attrs.loading ? 'loading...' : 'X'}
+                                                    </p>
+                                                </div>
                                             )
                                             }
                                         </Mutation>
@@ -73,5 +78,41 @@ const UserRecipes = ({username}) => {
     )
         ;
 };
+
+const EditRecipeModal = () => (
+    <div className="modal modal-open">
+        <div className="modal-inner">
+            <div className="modal-content">
+                <form className="modal-content-inner">
+                    <h4>Edit Recipe</h4>
+
+                    <input type="text"
+                           name="name"
+                           value={name}
+                           onChange={handleChange}
+                           placeholder="Recipe Name"/>
+                    <input type="text"
+                           name="imageUrl"
+                           value={imageUrl}
+                           onChange={handleChange}
+                           placeholder="Recipe Image"/>
+                    <select name="category"
+                            value={category}
+                            onChange={handleChange}>
+                        <option value="Breakfast">Breakfast</option>
+                        <option value="Lunch">Lunch</option>
+                        <option value="Dinner">Dinner</option>
+                        <option value="Snack">Snack</option>
+                    </select>
+                    <input type="text"
+                           name="description"
+                           value={description}
+                           onChange={handleChange}
+                           placeholder="Add descriptions"/>
+                </form>
+            </div>
+        </div>
+    </div>
+);
 
 export default UserRecipes;
--- client/src/index.js
+++ client/src/index.js
@@ -18,8 +18,8 @@ import AddRecipe from "./components/Recipe/AddRecipe";
 import RecipePage from "./components/Recipe/RecipePage";
 
 const client = new ApolloClient({
-    uri: "https://recipes-app-gql.herokuapp.com/graphql",
-    /*uri: "http://localhost:4444/graphql",*/
+    /*uri: "https://recipes-app-gql.herokuapp.com/graphql",*/
+    uri: "http://localhost:4444/graphql",
     fetchOptions: {
         credentials: true
     },
